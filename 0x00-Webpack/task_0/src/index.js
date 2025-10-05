5fcf20d6b28e93ee7974ef78085eee65e2a2abd7
Dennis Adaku <dennisadaku@ymail.com>
2025-10-05 13:37:03 +0100

    chore (task 0): Basic webpack setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+
+$(document).ready(function() {
+    const paragraphs = [
+        "ALX Dashboard",
+        "Dashboard data for the students",
+        "Copyright - ALX"
+    ];
+
+    paragraphs.forEach(paragraph => {
+        $("body").append(`<p>${text}</p>`);
+    });
+});
