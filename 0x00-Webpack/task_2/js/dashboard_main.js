05b752576ed9ac4c40fdb308a3dc44f31d286774
Dennis Adaku <dennisadaku@ymail.com>
2025-10-06 20:52:56 +0100

    task 2(Add css and images using webpack loaders

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+import _ from "lodash";
+import "../css/main.css";
+
+$(document).ready(function() {
+    // Add logo at the top
+    $("body").preppend("<div id='logo'></div>");
+
+    $("body").append("<p>ALX Dashboard</p>");
+    $("body").append("<p>Dashboard data for the students</p>");
+    $("body").append("<button'>Click here to get started</button>");
+    $("body").append("<p id='count'></p>");
+    $("body").append("<p>Copyright - ALX</p>");
+
+    let count = 0;
+
+    // Fuction to update click count
+    function UpdateCounter() {
+        count++;
+
+        $("#count").text(`${count} clicks on the button`);
+    }
+
+    // Lodash debounce to limit number of times clicks are counted
+    const debounceUpdate = _.debounce(UpdateCounter, 300);
+
+    // Bind event
+    $("button").on("click", debounceUpdate);
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+
+module.exports = {
+    entry: "./js/dashboard_main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public")
+    },
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[hash].[ext]",
+                            outputPath: "images"
+                        }
+                    },
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            mozjpeg: {progressive: true},
+                            optipng: {enable: true},
+                            pngquant: {quality: [0.65, 0.9], speed: 4}
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    performance: {
+        hints: false
+    }
+};
