2e6910de4c6793286105cbd02617ee1636de6eb6
Dennis Adaku <dennisadaku@ymail.com>
2025-10-19 12:38:00 +0100

    added REAME file

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     // Function to update click count
     function updateCounter() {
         count++;
-        $("#count").text(`${count} click on the button`);
+        $("#count").text(`${count} clicks on the button`);
     }
 
     // Lodash debounce to limit number of times clicks are counted
